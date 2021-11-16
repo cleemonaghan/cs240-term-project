546060e843e56168d67335b8fa36adf27b0fc95c
Eli Arao <earao@hotmail.com>
2021-11-15 20:00:10 -0800

    initial branch commit. Made a javascript file and put the general outline for a rubric. Need to make it a class

---
+++ graderScript.js
@@ -0,0 +1,17 @@
+
+
+function newRubric()
+{
+    // Get the dom that will house the rubric
+    dom = document.querySelector("#rubric");
+
+    // Create a new rubric
+    //  General idea:
+    //      -The rubric will be a table
+    //          -3 Columns
+    //              -Column one is for points: input box that will update total on change
+    //              -Column two is for category name
+    //              -Column three is for notes: text box that will be saved on press of a button?
+    //      -There will be a add new row button
+    //      -There will be a subtract row button for each row
+}
