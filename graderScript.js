1693930f8b472850be1529706a25fbc6f15462ee
Eli Arao <earao@hotmail.com>
2021-11-16 12:20:27 -0800

    Made basic no formatting rubric display. Can add new rows with button but delete only works for last added row right now. Need to look at potential better input boxes and re-evaluate general format before going foward. Needs parent div to hold shape and general css

--- graderScript.js
+++ graderScript.js
@@ -1,10 +1,82 @@
 
+newRubric();
 
 function newRubric()
 {
     // Get the dom that will house the rubric
-    dom = document.querySelector("#rubric");
+    div = document.querySelector("#rubric");
+    table = document.createElement("table");
+    div.appendChild(table);
+    
+    //
+    headerRow = document.createElement("tr");
+    
+    notesLabel = document.createElement("th");
+    notesLabel.innerHTML = "Points";
+    headerRow.appendChild(notesLabel);
+    
+    categoryLabel = document.createElement("th");
+    categoryLabel.innerHTML = "Category";
+    headerRow.appendChild(categoryLabel);
+    
+    notesLabel = document.createElement("th");
+    notesLabel.innerHTML = "Notes";
+    headerRow.appendChild(notesLabel);
+    
+    newRowButtonLabel = document.createElement("th");
+    addNewButton = document.createElement("button");
+    addNewButton.innerHTML = "Add New";
+    newRowButtonLabel.appendChild(addNewButton);
+    headerRow.appendChild(newRowButtonLabel);
 
+    table.appendChild(headerRow);
+
+    ////////////////////////////////////////////////////
+    
+    addNewButton.addEventListener("click", function()
+    {
+        newRow = document.createElement("tr");
+
+        // Points
+        pointsOutOf = document.createElement("td");
+        pointsOutOf.appendChild(document.createElement("input"));
+        pointsOutOf.innerHTML += "/";
+        pointsOutOf.appendChild(document.createElement("input"));
+
+        newRow.appendChild(pointsOutOf);
+
+        // Category
+        category = document.createElement("td");
+        category.appendChild(document.createElement("input"));
+
+        newRow.appendChild(category);
+
+        // Notes
+        comments = document.createElement("td");
+        comments.appendChild(document.createElement("input"));
+
+        newRow.appendChild(comments);
+
+        // Remove button
+        removeButton = document.createElement("button")
+        removeButton.innerHTML = "Delete Row";
+        remove = document.createElement("td");
+        remove.appendChild(removeButton);
+
+        // NEEDS FIX
+        removeButton.addEventListener("click", function()
+        {
+            table.removeChild(newRow);
+        });
+        //
+
+        newRow.appendChild(removeButton);
+
+        //
+        table.appendChild(newRow);
+    });
+
+    
     // Create a new rubric
     //  General idea:
     //      -The rubric will be a table
