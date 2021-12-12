4bc5e4b290326f4e28170695eb482acda9d37d6f
Colin Monaghan <cleemonaghan@gmail.com>
2021-12-11 19:08:41 -0800

    Created express.js file for sending html pages to the front end.

---
+++ express.js
@@ -0,0 +1,26 @@
+const port = 8102;
+const express = require("express");
+const path = require("path");
+const server = express();
+
+server.use(express.static(__dirname + "/public"));
+
+server.listen(port, function () {
+	console.log(`The server is now running on ${port}`);
+});
+
+server.get("/grade", function (req, resp) {
+	console.log(`Got an http GET request for /grade`);
+	console.log(`Query String: ${req.query}`);
+
+	resp.sendFile(path.join(__dirname, "/grade/grade.html"));
+});
+
+server.post("/login", function (req, resp) {
+	console.log("Got an http POST request to /login");
+});
+
+// for any other route, we will catch it with this
+server.get("*", function (req, resp) {
+	console.log("Got a request for an unsupported route");
+});
