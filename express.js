d3dda3670269720b3b4fc38d8153f008b856f261
Colin Monaghan <cleemonaghan@gmail.com>
2021-12-11 19:49:02 -0800

    Fixed path error in express.js

--- express.js
+++ express.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const server = express();
 
-server.use(express.static(__dirname + "/public"));
+server.use(express.static(__dirname + "/grade/public"));
 
 server.listen(port, function () {
 	console.log(`The server is now running on ${port}`);
