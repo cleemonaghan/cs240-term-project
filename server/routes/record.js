4e388d83ad15b87fb2621fb5b84f4fce77d698fa
Colin Monaghan <cleemonaghan@gmail.com>
2021-12-12 23:39:00 -0800

    Updated routes

--- server/routes/record.js
+++ server/routes/record.js
@@ -10,25 +10,6 @@ const recordRoutes = express.Router();
 // This will help us connect to the database
 const dbo = require("../db/conn");
 
-// This section will help you get a list of all the assignments
-recordRoutes.route("/assignments/fetchAll").get(async function (req, res) {
-	//connect to the database
-	const dbConnect = dbo.getDb();
-
-	const matchDocument = req.query;
-	dbConnect
-		.collection("assignments")
-		.find(matchDocument)
-		.limit(50)
-		.toArray(function (err, result) {
-			if (err) {
-				res.status(400).send("Error fetching assignments!");
-			} else {
-				res.json(result);
-			}
-		});
-});
-
 // This section will help you validate a user's credentials
 recordRoutes.route("/users/validate").get(async function (req, res) {
 	//connect to the database
@@ -42,14 +23,13 @@ recordRoutes.route("/users/validate").get(async function (req, res) {
 			if (err) {
 				res.json(null);
 			} else {
-				//console.log(result);
 				res.json(result);
 			}
 		});
 });
 
 // This section will help you validate a user's credentials
-recordRoutes.route("/users/add").get(async function (req, res) {
+recordRoutes.route("/users/add").post(async function (req, res) {
 	//connect to the database
 	const dbConnect = dbo.getDb();
 	const matchDocument = req.body.params;
@@ -80,6 +60,7 @@ recordRoutes.route("/users/delete").delete((req, res) => {
 					.send(`Error deleting listing with id ${listingQuery.username}!`);
 			} else {
 				console.log("1 user deleted");
+				res.status(204).send();
 			}
 		});
 });
@@ -103,6 +84,25 @@ recordRoutes.route("/assignments").get(async function (req, res) {
 		});
 });
 
+// This section will help you get a list of all the assignments
+recordRoutes.route("/assignments/fetchAll").get(async function (req, res) {
+	//connect to the database
+	const dbConnect = dbo.getDb();
+
+	const matchDocument = req.query;
+	dbConnect
+		.collection("assignments")
+		.find(matchDocument)
+		.limit(50)
+		.toArray(function (err, result) {
+			if (err) {
+				res.status(400).send("Error fetching assignments!");
+			} else {
+				res.json(result);
+			}
+		});
+});
+
 // This section will help you create a new record.
 recordRoutes.route("/assignments/create").post(function (req, res) {
 	const dbConnect = dbo.getDb();
@@ -115,6 +115,36 @@ recordRoutes.route("/assignments/create").post(function (req, res) {
 				res.status(400).send("Error inserting matches!");
 			} else {
 				console.log(`Added a new match with id ${result.insertedId}`);
+				res.status(204).send();
+			}
+		});
+});
+
+recordRoutes.route("/assignments/submitAssignment").post(function (req, res) {
+	const dbConnect = dbo.getDb();
+	const params = req.body.params;
+
+	const listingQuery = {
+		class: params.class,
+		studentID: params.studentID,
+	};
+	const updates = {
+		$set: {
+			text: params.text,
+			highestID: params.highestID,
+			comments: params.comments,
+			maxPoints: params.maxPoints,
+			rows: params.rows,
+		},
+	};
+
+	dbConnect
+		.collection("assignments")
+		.updateOne(listingQuery, updates, function (err, _result) {
+			if (err) {
+				res.status(400).send(`Error submitting assignment!`);
+			} else {
+				console.log("1 document updated");
 				res.status(204).send();
 			}
 		});
@@ -148,6 +178,7 @@ recordRoutes.route("/assignments/updateAssignment").post(function (req, res) {
 					);
 			} else {
 				console.log("1 document updated");
+				res.status(204).send();
 			}
 		});
 });
@@ -176,6 +207,7 @@ recordRoutes.route("/assignments/updateComments").post(function (req, res) {
 					);
 			} else {
 				console.log("1 document updated");
+				res.status(204).send();
 			}
 		});
 });
@@ -183,7 +215,7 @@ recordRoutes.route("/assignments/updateComments").post(function (req, res) {
 // This section will help you update a record by id.
 recordRoutes.route("/assignments/updateRubric").post(function (req, res) {
 	const dbConnect = dbo.getDb();
-	console.log(req.body);
+	//console.log(req.body);
 	const listingQuery = {
 		class: req.body.params.class,
 		studentID: req.body.params.studentID,
@@ -195,8 +227,8 @@ recordRoutes.route("/assignments/updateRubric").post(function (req, res) {
 		},
 	};
 
-	console.log(listingQuery);
-	console.log(updates);
+	//console.log(listingQuery);
+	//console.log(updates);
 	dbConnect
 		.collection("assignments")
 		.updateOne(listingQuery, updates, function (err, _result) {
@@ -206,6 +238,7 @@ recordRoutes.route("/assignments/updateRubric").post(function (req, res) {
 					.send(`Error updating for student ${listingQuery.studentID}!`);
 			} else {
 				console.log("1 document updated");
+				res.status(204).send();
 			}
 		});
 });
@@ -224,6 +257,7 @@ recordRoutes.route("/assignments/delete").delete((req, res) => {
 					.send(`Error deleting listing with id ${listingQuery.studentID}!`);
 			} else {
 				console.log("1 document deleted");
+				res.status(204).send();
 			}
 		});
 });
