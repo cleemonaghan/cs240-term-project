abf5b425a812589b747e451292ee3f073de4cfe0
Colin Monaghan <cleemonaghan@gmail.com>
2021-12-12 19:40:34 -0800

    Updated paths in express.js

--- express.js
+++ express.js
@@ -3,8 +3,10 @@ const express = require("express");
 const path = require("path");
 const server = express();
 
+//Grant access to all the public directories
 server.use(express.static(__dirname + "/grade/public"));
 server.use(express.static(__dirname + "/session/public"));
+server.use(express.static(__dirname + "/dashboard/public"));
 
 server.listen(port, function () {
 	console.log(`The server is now running on ${port}`);
@@ -16,10 +18,16 @@ server.get("/grade", function (req, resp) {
 	resp.sendFile(path.join(__dirname, "/grade/grade.html"));
 });
 
-server.post("/login", function (req, resp) {
-	console.log("Got an http POST request to /login");
+server.get("/login", function (req, resp) {
+	console.log("Got an http GET request to /login");
 
 	resp.sendFile(path.join(__dirname, "/session/login.html"));
+});
+
+server.get("/dashboard", function (req, resp) {
+	console.log("Got an http GET request to /dashboard");
+
+	resp.sendFile(path.join(__dirname, "/dashboard/dashboard.html"));
 });
 
 // for any other route, we will catch it with this
